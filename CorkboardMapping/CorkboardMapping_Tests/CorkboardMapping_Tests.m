f1c67d47b74ed0f748659d9ccc58c07660a0dd3b
Ashley Patterson <ash.patterson@social-mobiles.com>
2014-03-01 11:41:07 -0800

    Still had trouble with tests compiling, removed old test target and created a new one.

---
+++ CorkboardMapping/CorkboardMapping_Tests/CorkboardMapping_Tests.m
@@ -0,0 +1,34 @@
+//
+//  CorkboardMapping_Tests.m
+//  CorkboardMapping_Tests
+//
+//  Created by Ashley Patterson on 3/1/14.
+//  Copyright (c) 2014 Ashley Patterson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CorkboardMapping_Tests : XCTestCase
+
+@end
+
+@implementation CorkboardMapping_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
