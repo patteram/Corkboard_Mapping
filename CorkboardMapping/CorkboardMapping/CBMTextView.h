41c4cda8c8c24c4a71829f69c107f40693f83390
Ashley Patterson <ash.patterson@social-mobiles.com>
2014-04-21 15:27:26 -0700

    Textview allows mouse down events to go to superview.

---
+++ CorkboardMapping/CorkboardMapping/CBMTextView.h
@@ -0,0 +1,13 @@
+//
+//  CBMTextView.h
+//  CorkboardMapping
+//
+//  Created by Ashley Patterson on 4/20/14.
+//  Copyright (c) 2014 Ashley Patterson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CBMTextView : NSTextView
+
+@end
---
+++ CorkboardMapping/CorkboardMapping/CBMTextView.m
@@ -0,0 +1,29 @@
+//
+//  CBMTextView.m
+//  CorkboardMapping
+//
+//  Created by Ashley Patterson on 4/20/14.
+//  Copyright (c) 2014 Ashley Patterson. All rights reserved.
+//
+
+#import "CBMTextView.h"
+
+@implementation CBMTextView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
